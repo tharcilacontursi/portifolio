b85f2d62a8296d94d341060ac6d52c042593feaf
Tharcila Contursi <tharcilakcontursi@gmail.com>
2024-03-29 15:51:12 +0100

    estilizando sideBar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,6 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/tharcilacontursi.png" alt="" />
+)
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,17 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/tharcilacontursi.png"></img>
-    <Title fontSize={20}>Tharcila Contursi </Title>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Title fontSize={20}>Tharcila Contursi </Title>
+      <Paragrafo tipo="secundario">tharcilacontursi</Paragrafo>
+      <Descricao tipo="principal">Desenvolvedora Web</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
